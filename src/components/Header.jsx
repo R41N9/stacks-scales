ad25c0e48736a7c470032c4cc1f67053f2120593
Iain Furstenwerth <iain.furstenwerth@gmail.com>
2021-11-21 12:51:36 -0600

    added more mobile styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,40 +21,34 @@ class Header extends React.Component {
         </Grid>
         <Grid
           item xs={3}
+          className="mobile-only"
+        >
+          <div className="dropdown">
+            <button id="menu-button" className="nav-button">&#8801;</button>
+            <div className="dropdown-content">
+              <div className="dropdown">
+                <button className="nav-button">ARTISTS &#x25BE;</button>
+                <div className="artist-dropdown-content">
+                  <button id="youngod-button" className="artist-button nav-button" onClick={ (e) => this.props.handleArtistClick(e) }>YounGod</button>
+                  <button id="maria-button" className="artist-button nav-button" onClick={ (e) => this.props.handleArtistClick(e) }>Maria</button>
+                  <button id="luke-button" className="artist-button nav-button" onClick={ (e) => this.props.handleArtistClick(e) }>Luke</button>
+                  <button id="darien-button" className="artist-button nav-button" onClick={ (e) => this.props.handleArtistClick(e) }>Darien</button>
+                  <button id="michael-button" className="artist-button nav-button" onClick={ (e) => this.props.handleArtistClick(e) }>Michael</button>
+                </div>
+              </div>
+              <button onClick={ () => this.props.handleContactClick() } className="nav-button">CONTACT</button>
+            </div>
+          </div>
+        </Grid>
+        <Grid
+          item xs={3}
           container
           direction="row"
           justifyContent="space-evenly"
           alignItems="flex-end"
+          className="desktop-only"
         >
-          <Grid
-            item xs={3}
-            className="mobile-only"
-          >
-            <div className="dropdown">
-              <button className="nav-button">&#8801;</button>
-              <div className="dropdown-content">
-                <Grid item xs={12} md={3}>
-                  <div className="dropdown">
-                    <button className="nav-button">ARTISTS &#x25BE;</button>
-                    <div className="dropdown-content">
-                      <button id="youngod-button" className="artist-button nav-button" onClick={ (e) => this.props.handleArtistClick(e) }>YounGod</button>
-                      <button id="maria-button" className="artist-button nav-button" onClick={ (e) => this.props.handleArtistClick(e) }>Maria</button>
-                      <button id="luke-button" className="artist-button nav-button" onClick={ (e) => this.props.handleArtistClick(e) }>Luke</button>
-                      <button id="darien-button" className="artist-button nav-button" onClick={ (e) => this.props.handleArtistClick(e) }>Darien</button>
-                      <button id="michael-button" className="artist-button nav-button" onClick={ (e) => this.props.handleArtistClick(e) }>Michael</button>
-                    </div>
-                  </div>
-                </Grid>
-                <Grid item xs={12} md={3}>
-                  <button onClick={ () => this.props.handleContactClick() } className="nav-button">CONTACT</button>
-                </Grid>
-              </div>
-            </div>
-          </Grid>
-          <Grid
-            item xs={12} md={3}
-            className="desktop-only"
-          >
+          <Grid item xs={4}>
             <div className="dropdown">
               <button className="nav-button">ARTISTS &#x25BE;</button>
               <div className="dropdown-content">
@@ -66,7 +60,7 @@ class Header extends React.Component {
               </div>
             </div>
           </Grid>
-          <Grid item xs={12} md={3}>
+          <Grid item xs={3}>
             <button onClick={ () => this.props.handleContactClick() } className="nav-button">CONTACT</button>
           </Grid>
         </Grid>
