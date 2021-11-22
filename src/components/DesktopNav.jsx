06223b7db01ed876bda22d881d62e3ca8f6d437e
Iain Furstenwerth <iain.furstenwerth@gmail.com>
2021-11-21 21:26:03 -0600

    added grid column to menu and dd content

--- src/components/DesktopNav.jsx
+++ src/components/DesktopNav.jsx
@@ -17,20 +17,69 @@ class DesktopNav extends React.Component {
         justifyContent="space-evenly"
         alignItems="flex-end"
       >
-        <Grid item xs={5}>
+        <Grid
+          item xs={5}
+          container
+          spacing={1}
+          direction="column"
+          justifyContent="flex-end"
+          align-items="flex-start"
+        >
           <div className="artist-dropdown dropdown">
-            <button className="nav-button">ARTISTS &#x25BE;</button>
+            <Grid item xs={12}>
+              <button className="nav-button">ARTISTS &#x25BE;</button>
+            </Grid>
             <div className="artist-dropdown-content dropdown-content">
-              <button id="youngod-button" className="artist-button nav-button" onClick={ (e) => this.props.handleArtistClick(e) }>YounGod</button>
-              <button id="maria-button" className="artist-button nav-button" onClick={ (e) => this.props.handleArtistClick(e) }>Maria</button>
-              <button id="luke-button" className="artist-button nav-button" onClick={ (e) => this.props.handleArtistClick(e) }>Luke</button>
-              <button id="darien-button" className="artist-button nav-button" onClick={ (e) => this.props.handleArtistClick(e) }>Darien</button>
-              <button id="michael-button" className="artist-button nav-button" onClick={ (e) => this.props.handleArtistClick(e) }>Michael</button>
+              <Grid item xs={12}>
+                <button
+                  id="youngod-button"
+                  className="artist-button nav-button"
+                  onClick={ (e) => this.props.handleArtistClick(e) }>
+                    YounGod
+                </button>
+              </Grid>
+              <Grid item xs={12}>
+                <button
+                  id="maria-button"
+                  className="artist-button nav-button"
+                  onClick={ (e) => this.props.handleArtistClick(e) }>
+                    Maria
+                </button>
+              </Grid>
+              <Grid item xs={12}>
+                <button
+                  id="luke-button"
+                  className="artist-button nav-button"
+                  onClick={ (e) => this.props.handleArtistClick(e) }>
+                    Luke
+                </button>
+              </Grid>
+              <Grid item xs={12}>
+                <button
+                  id="darien-button"
+                  className="artist-button nav-button"
+                  onClick={ (e) => this.props.handleArtistClick(e) }>
+                    Darien
+                </button>
+              </Grid>
+              <Grid item xs={12}>
+                <button
+                  id="michael-button"
+                  className="artist-button nav-button"
+                  onClick={ (e) => this.props.handleArtistClick(e) }>
+                    Michael
+                </button>
+              </Grid>
             </div>
           </div>
         </Grid>
         <Grid item xs={3}>
-          <button id="contact-button" onClick={ () => this.props.handleContactClick() } className="nav-button">CONTACT</button>
+          <button
+            id="contact-button"
+            onClick={ () => this.props.handleContactClick() }
+            className="nav-button">
+            CONTACT
+          </button>
         </Grid>
       </Grid>
     )
--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -10,17 +10,40 @@ class MobileNav extends React.Component {
 
   render() {
     return (
-      <Grid item xs={3}>
+      <Grid
+          item xs={3}
+          container
+          spacing={1}
+          direction="column"
+          justifyContent="flex-end"
+          align-items="flex-start"
+        >
         <div className="mobile-dropdown dropdown">
-          <button id="menu-button" className="nav-button">&#8801;</button>
+          <Grid item xs={12}>
+            <button id="menu-button" className="nav-button">&#8801;</button>
+          </Grid>
           <div className="mobile-dropdown-content dropdown-content">
-            <button className="nav-button">ARTISTS &#x25BE;</button>
-            <button id="youngod-button" className="artist-button nav-button" onClick={ (e) => this.props.handleArtistClick(e) }>YounGod</button>
-            <button id="maria-button" className="artist-button nav-button" onClick={ (e) => this.props.handleArtistClick(e) }>Maria</button>
-            <button id="luke-button" className="artist-button nav-button" onClick={ (e) => this.props.handleArtistClick(e) }>Luke</button>
-            <button id="darien-button" className="artist-button nav-button" onClick={ (e) => this.props.handleArtistClick(e) }>Darien</button>
-            <button id="michael-button" className="artist-button nav-button" onClick={ (e) => this.props.handleArtistClick(e) }>Michael</button>
-            <button onClick={ () => this.props.handleContactClick() } className="nav-button">CONTACT</button>
+            <Grid item xs={12}>
+              <button className="nav-button">ARTISTS &#x25BE;</button>
+            </Grid>
+            <Grid item xs={12}>
+              <button id="youngod-button" className="artist-button nav-button" onClick={ (e) => this.props.handleArtistClick(e) }>YounGod</button>
+            </Grid>
+            <Grid item xs={12}>
+              <button id="maria-button" className="artist-button nav-button" onClick={ (e) => this.props.handleArtistClick(e) }>Maria</button>
+            </Grid>
+            <Grid item xs={12}>
+              <button id="luke-button" className="artist-button nav-button" onClick={ (e) => this.props.handleArtistClick(e) }>Luke</button>
+            </Grid>
+            <Grid item xs={12}>
+              <button id="darien-button" className="artist-button nav-button" onClick={ (e) => this.props.handleArtistClick(e) }>Darien</button>
+            </Grid>
+            <Grid item xs={12}>
+              <button id="michael-button" className="artist-button nav-button" onClick={ (e) => this.props.handleArtistClick(e) }>Michael</button>
+            </Grid>
+            <Grid item xs={12}>
+              <button onClick={ () => this.props.handleContactClick() } className="nav-button">CONTACT</button>
+            </Grid>
           </div>
         </div>
       </Grid>
