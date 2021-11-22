fe9386cf467046d641efac90735028cc632ec273
Iain Furstenwerth <iain.furstenwerth@gmail.com>
2021-11-21 22:57:30 -0600

    finally fixed the header issue, hopefully. works on mobile version in devtools

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -10,40 +10,17 @@ class MobileNav extends React.Component {
 
   render() {
     return (
-      <Grid
-          item xs={3}
-          container
-          spacing={1}
-          direction="column"
-          justifyContent="center"
-          align-items="flex-start"
-        >
+      <Grid item xs={3}>
         <div className="mobile-dropdown dropdown">
-          <Grid item xs={12}>
-            <button id="menu-button" className="nav-button">&#8801;</button>
-          </Grid>
+          <button id="menu-button" className="nav-button">&#8801;</button>
           <div className="mobile-dropdown-content dropdown-content">
-            <Grid item xs={12}>
               <button className="nav-button">ARTISTS &#x25BE;</button>
-            </Grid>
-            <Grid item xs={12}>
               <button id="youngod-button" className="artist-button nav-button" onClick={ (e) => this.props.handleArtistClick(e) }>YounGod</button>
-            </Grid>
-            <Grid item xs={12}>
               <button id="maria-button" className="artist-button nav-button" onClick={ (e) => this.props.handleArtistClick(e) }>Maria</button>
-            </Grid>
-            <Grid item xs={12}>
               <button id="luke-button" className="artist-button nav-button" onClick={ (e) => this.props.handleArtistClick(e) }>Luke</button>
-            </Grid>
-            <Grid item xs={12}>
               <button id="darien-button" className="artist-button nav-button" onClick={ (e) => this.props.handleArtistClick(e) }>Darien</button>
-            </Grid>
-            <Grid item xs={12}>
               <button id="michael-button" className="artist-button nav-button" onClick={ (e) => this.props.handleArtistClick(e) }>Michael</button>
-            </Grid>
-            <Grid item xs={12}>
               <button onClick={ () => this.props.handleContactClick() } className="nav-button">CONTACT</button>
-            </Grid>
           </div>
         </div>
       </Grid>
